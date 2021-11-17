370ddadc3fdbd4c8b17da5d33365a56c14ca7863
12200454lorensabere <lorensabere1116@gmail.com>
2021-11-17 16:37:27 +0700

    tugas pertemuan 2

---
+++ app/Views/halaman/daftar_member.php
@@ -0,0 +1,10 @@
+    <html>
+        <head>
+            <title>Daftar member</title>
+    </head>
+    <body>
+        <hl>Hai.. kamu mau menjadi menjadi member?</hl>
+        <pi>Isikan data diri kamu dulu ya agar kami kenal dengan member kami</pl>
+    </body>
+
+</html>
---
+++ app/Views/halaman/login.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Perpustakaan Login</title>
+</head>
+<body>
+
+    <hl>Selamat datang di Sistem Informasi Perpustakaan</hl>
+    <p>
+        Ini adalah layanan perpustakaan digital masa
+        <a href="<?=base_url('/daftar_member')?>" >klik daftar</a> untuk mendaftar menjadi member
+    </p>
+
+    </body>
+</html>
